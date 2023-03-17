6dfc4163ef57079e82c17ba9fba7228850c00f77
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-03-17 04:12:27 +0200

    hello c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,23 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - main block getting size
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char c;
+	float f;
+	long int l;
+	long long int ll;
+
+	printf("Size of an int %d byte(s)", sizeof(i));
+	printf("Size of a char %c byte(s)", sizeof(c));
+	printf("Size of a float %f byte(s)", sizeof(f));
+	printf("Size of a long int %ld byte(s)", sizeof(l));
+	printf("Size of a long long int %lld byte(s)", sizeof(ll));
+	return (0);
+}
+
