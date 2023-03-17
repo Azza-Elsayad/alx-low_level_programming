0e879274db5d43e304909b63afd3101482504716
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-03-17 03:18:43 +0200

    hello c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - main contains printf function to print a string
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
