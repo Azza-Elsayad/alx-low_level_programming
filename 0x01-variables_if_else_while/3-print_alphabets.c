d9f331ac79f1e5a8dfb3e1d4d521c80837e7f52e
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-03-18 02:47:39 +0200

    hello c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+/**
+ * main - printing alphabet.
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a' ; l <= 'z'; l ++)
+	{
+		putchar(l);
+	}
+	putchar(\n);
+	return (0);
+}
