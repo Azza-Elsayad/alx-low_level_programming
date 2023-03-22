7753ce451052c669da322494e62087554ed47bfb
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-03-22 13:47:17 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main - printing alphabet.
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a' ; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
