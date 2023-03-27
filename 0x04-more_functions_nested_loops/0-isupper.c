b805ada7d160ec0ec209f88fafe4932655a0c2aa
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-03-27 12:21:33 +0200

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * _isupper - return 1 if uppercase alphabet and 0 if lowercase
+ * @c: takes in a character
+ * Return: 1 for uppercase, 0 otherwise
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
