7dfe1ec9c24d491556bcab4e9a1b36dbfcff698b
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-03-29 15:57:29 +0200

    hello c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+ #include "main.h"
+/**
+ *rev_string - print string in revers
+ *@s: the pointer that poiint to the first element of the string
+ *Return: string
+ **/
+void rev_string(char *s);
+{
+	int i;
+
+for (i = 0 ; s[i] != '\0'; i++)
+	{
+	}
+	for (i = i - 1 ; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+		_putchar('\n');
+}
