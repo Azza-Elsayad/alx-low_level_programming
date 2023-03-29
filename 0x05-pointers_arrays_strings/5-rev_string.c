5353d1ff7c4b6c1e7b08856f1771ec144eaa9ec6
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-03-29 16:06:59 +0200

    hello c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,10 @@ void rev_string(char *s)
 {
 	int i;
 
-for (i = 0 ; s[i] != '\0'; i++)
+	for (i = 0 ; s[i] != '\0'; i++)
 	{
 		_putchar(s[i]);
 	}
-		_putchar('\n');
 	for (i = i - 1 ; i >= 0 ; i--)
 	{
 		_putchar(s[i]);
