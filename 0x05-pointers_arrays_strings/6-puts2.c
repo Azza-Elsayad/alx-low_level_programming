e49821f0746a174f44d27997870c4614827d1259
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-04-07 16:40:05 +0200

    hello c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ *puts2 - prints every other letter in the string
+ *@str: to check.
+ *Returne 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i != \0; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar('str[i]);
+		}
+	}
+}
