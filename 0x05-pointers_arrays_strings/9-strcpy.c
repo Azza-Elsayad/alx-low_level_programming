f02162c0106c8c9089dca22ab64c4fbaa62e6468
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-04-08 23:57:20 +0200

    scrdest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ **_strcpy - copy string
+ *@dest: pointer points to a buffer destination
+ *@src: pointer points to string source
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	char string;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	string = +scr[i];
+	}
+	string = +scr[i];
+	*dest = string;
+	return (dest);
+}
+
