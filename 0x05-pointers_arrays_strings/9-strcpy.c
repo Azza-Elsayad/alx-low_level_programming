9973bbbc8862b9d7f06ac18b8175a31214ae919b
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-04-08 23:59:44 +0200

    scrdest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,9 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-	string = +scr[i];
+	string = +src[i];
 	}
-	string = +scr[i];
+	string = +src[i];
 	*dest = string;
 	return (dest);
 }
