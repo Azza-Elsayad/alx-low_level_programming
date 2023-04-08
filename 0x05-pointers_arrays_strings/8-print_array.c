4e490d7fd21db43ad7b828b1382a9beea452a776
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-04-08 17:39:54 +0200

    hello c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *puts_half - print half string
+ *@str: char to print half
+ *Return 0
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	for (j = (i + 1) / 2 ; j < i ; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
+
