685bbf4a1048dd446301a0c6a4cf69c0982770b4
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-04-10 00:24:27 +0200

    2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+*main - print args
+*@argc: cont of arg
+*@argv: pointer to arry of string
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+{
+return (0);
+}
