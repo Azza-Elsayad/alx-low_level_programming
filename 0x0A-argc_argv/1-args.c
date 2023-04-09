323f5754c7c0dea681c6ea0583670395386125a5
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-04-10 00:16:58 +0200

    1-args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+*main - check arg
+*@argc: cont of arg
+*@argv: pointer to arry of string
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+(void)argv;
+return (0);
+}
