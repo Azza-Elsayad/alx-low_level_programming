6ff65732d3213b6dcb26544f02d528414ea8d4fa
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-04-10 00:13:20 +0200

    mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-*
-*
-*
+*main - check arg
+*@argc: cont of arg
+*@argv: pointer to arry of string
+*Return: 0
 */
 int main(int argc, char *argv[])
 {
-printf("%s\n",argv[0]);
+printf("%s\n", argv[0]);
 (void)argc;
 return (0);
 }
