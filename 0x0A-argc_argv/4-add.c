0cd1ae770a9e5fc303f28d169cb0b134756082dc
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-04-11 00:27:49 +0200

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,40 @@
-#!/bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Adds args
+*@argc: size of argv arry
+*@argv: pointer to arry of string
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int sum = 0;
+int i = 1;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+
+else
+{
+/*while(isdigit argv[i] == true)*/
+
+for (i = 1; i < argc; i++)
+{
+	if (*argv[i] < '0' || *argv[i] >'9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+sum += atoi(argv[i]);
+			}		
+}
+}
+
+printf("%d\n", sum);
+
+return (1);
+}
