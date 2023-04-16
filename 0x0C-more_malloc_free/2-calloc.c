d85689c6524404119856e199a753edde94987fa9
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-04-16 17:52:26 +0200

    c malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ char *p;
 if (nmemb == 0 || size == 0)
 	return (NULL);
 p = malloc(nmemb * size);
-for (i = 0; i < nmemb; i++)
+for (i = 0; i < nmemb * size; i++)
 	p[i] = 0;
 if (p == NULL)
 	return (NULL);
