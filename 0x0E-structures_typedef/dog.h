5cff06a55a067f7d19bb7781db26f08cc1e24817
Azza-Elsayad <azzaelsayad2011@gmail.com>
2023-04-18 15:03:09 +0200

    hello c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ *struct dog - dog data
+ *@name: name of dog
+ *@age: its age
+ *@owner: name of its owner
+ *Descrebtion: dog data
+ */
+typedef struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+
+#endf
